0edb4d9bb478c65a86a25210c86a892bc31dfa93
Jali Rainio <jali.rainio@sofokus.com>
2020-09-14 14:51:15 +0300

    1.3.

--- osa1/src/components/Content.js
+++ osa1/src/components/Content.js
@@ -3,7 +3,7 @@ import Part from './Part'
 
 class Content extends Component {
   render() {
-    return this.props.courseExerciseMap.map((value, index) => {
+    return this.props.courseParts.map((value, index) => {
       return (
         <Part partName={value[0]} partExerciseAmount={value[1]} />
       )
--- osa1/src/index.js
+++ osa1/src/index.js
@@ -6,24 +6,31 @@ import Total from './components/Total.js';
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const courseExerciseMap = [
-    [part1, exercises1],
-    [part2, exercises2],
-    [part3, exercises3]
+
+  const courseParts = [
+    [part1.name, part1.exercises],
+    [part2.name, part2.exercises],
+    [part3.name, part3.exercises]
   ]
 
   return (
     <div>
       <Header course={course} />
-      <Content courseExerciseMap={courseExerciseMap} />
-      <Total allExercises={[exercises1, exercises2, exercises3]} />
+      <Content courseParts={courseParts} />
+      <Total allExercises={[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   )
 }
