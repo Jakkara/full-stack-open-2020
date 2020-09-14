4a8bd045ea56020b6768f3dabdb2a98cb1ec78c6
Jali Rainio <jali.rainio@sofokus.com>
2020-09-14 15:03:05 +0300

    1.5.

--- osa1/src/components/Header.js
+++ osa1/src/components/Header.js
@@ -1,11 +1,10 @@
-
 import React, { Component } from 'react'
 
 class Header extends Component {
   render() {
     return (
       <div className="page-header">
-        <h1>{this.props.course}</h1>
+        <h1>{this.props.title}</h1>
       </div>
     )
   }
