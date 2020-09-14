f2809978dea568fd846de63db28bba082b1d4575
Jali Rainio <jali.rainio@sofokus.com>
2020-09-14 14:51:37 +0300

    Add missing file

---
+++ osa1/src/components/Part.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Part extends Component {
+  render() {
+    return (
+      <p>{this.props.partName + " - " + this.props.partExerciseAmount}</p>
+    )
+
+  }
+}
+
+export default Part 
