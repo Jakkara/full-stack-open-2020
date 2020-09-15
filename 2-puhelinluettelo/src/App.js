4af15ea0556e2311f67e9ef02d4a7e3e946f137f
Jali Rainio <jali.rainio@sofokus.com>
2020-09-15 16:55:38 +0300

    2.6

--- 2-puhelinluettelo/src/App.js
+++ 2-puhelinluettelo/src/App.js
@@ -6,19 +6,34 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = event => {
+    setNewName(event.target.value)
+  }
+
+  const handleNameSubmit = event => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleNameSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
     </div>
   )
 
