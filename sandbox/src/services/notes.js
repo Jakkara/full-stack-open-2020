d2f365ca5bb7b760fa114f9c75a0e9897d2cb9bd
Jali Rainio <jali.rainio@sofokus.com>
2020-09-18 18:43:50 +0300

    Update parent

--- sandbox/src/services/notes.js
+++ sandbox/src/services/notes.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-//const baseUrl = 'http://localhost:3001/api/notes'
-const baseUrl = 'http://cryptic-brushlands-68281.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
